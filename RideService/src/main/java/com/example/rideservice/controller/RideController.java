ac7af9d26c9d0c8c7ca8fe61d070d26545eedd53
vincent <lipinskivincent@gmail.com>
2023-09-14 15:00:17 +0200

    TpFinal

--- RideService/src/main/java/com/example/rideservice/controller/RideController.java
+++ RideService/src/main/java/com/example/rideservice/controller/RideController.java
@@ -25,25 +25,26 @@ public class RideController {
 
 
     @PostMapping("")
-    public ResponseEntity<Ride> postRide( @RequestParam Date date,@RequestParam String depart, @RequestParam String arrival,@RequestParam int userId,  @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
-//        RestClient<String, String> restClient = new RestClient<>();
-//        UserDTO userDTO = getUser(userId, token).getBody().getUserDTO();
-//        if(restClient.testToken(token, String.class) && userDTO.isDriver()) {
+    public ResponseEntity<Ride> postRide(@RequestParam Date date, @RequestParam String depart, @RequestParam String arrival, @RequestParam int userId, @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        RestClient<String, String> restClient = new RestClient<>();
+        UserDTO userDTO = getUser(userId, token).getBody().getUserDTO();
+        if(restClient.testToken(token, String.class) && userDTO.isDriver()) {
             Ride ride = rideService.createRide(date,depart,arrival,userId);
             return ResponseEntity.ok(ride);
-//        }
-//        return ResponseEntity.status(401).body(null);
+        }
+        return ResponseEntity.status(401).body(null);
     }
+
 
     @GetMapping("")
     public ResponseEntity<List<Ride>> getAllRide(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
-//        RestClient<String, String> restClient = new RestClient<>();
-//
-//        if(restClient.testToken(token, String.class)) {
+        RestClient<String, String> restClient = new RestClient<>();
+
+        if(restClient.testToken(token, String.class)) {
             List<Ride> rides = rideService.getAllRide();
             return ResponseEntity.ok(rides);
-//        }
-//        return ResponseEntity.status(401).body(null);
+        }
+        return ResponseEntity.status(401).body(null);
     }
 
     @GetMapping("user/{userId}")
